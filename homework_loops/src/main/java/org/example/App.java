06d12782307388bb577db4c003e1ea39a8fbbd6b
BeaS84 <131193427+BeaS84@users.noreply.github.com>
2023-04-19 21:06:24 +0200

    Added solution to the first task

---
+++ homework_loops/src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+//Ćwiczenia:
+//        Wypisanie liczb od 1 do 10 za pomocą pętli while
+//        Wypisanie liczb od 1 do 10 za pomocą pętli for
+//        Wypisanie liczb parzystych od 0 do 20 za pomocą pętli for
+//        Wypisanie liczb nieparzystych od 100 do 0 za pomocą pętli for
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //Zadanie 1.Wypisanie liczb od 1 do 10 za pomocą pętli while
+
+        Integer number = 1;// Why 1? if number == 0 than starts print from 0, not from 1
+
+        while (number<11){
+            System.out.println("wartość zmiennej number to: "+ number);
+            number++;
+        }
+
+    }
+}
