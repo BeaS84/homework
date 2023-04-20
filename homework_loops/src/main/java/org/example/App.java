bfd128c5e85e93ab220d1627ff5f8778907edc15
BeaS84 <131193427+BeaS84@users.noreply.github.com>
2023-04-20 16:49:44 +0200

    Solution to all tasks added

--- homework_loops/src/main/java/org/example/App.java
+++ homework_loops/src/main/java/org/example/App.java
@@ -6,22 +6,33 @@ package org.example;
 //        Wypisanie liczb parzystych od 0 do 20 za pomocą pętli for
 //        Wypisanie liczb nieparzystych od 100 do 0 za pomocą pętli for
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         //Zadanie 1.Wypisanie liczb od 1 do 10 za pomocą pętli while
 
         Integer number = 1;// Why 1? if number == 0 than starts print from 0, not from 1
 
-        while (number<11){
-            System.out.println("wartość zmiennej number to: "+ number);
+        while (number < 11) {
+            System.out.println("wartość zmiennej number to: " + number);
             number++;
         }
-        
-       // Zadanie 2 Wypisanie liczb od 1 do 10 za pomocą pętli while
-        
-        for ( Integer i = 1; i < 11; i++){
-            System.out.println("Wypisuję liczbę: "+ i);
+
+        // Zadanie 2 Wypisanie liczb od 1 do 10 za pomocą pętli while
+
+        for (Integer i = 1; i < 11; i++) {
+            System.out.println("Wypisuję liczbę: " + i);
+        }
+
+        //Zadanie 3: Wypisanie liczb parzystych od 0 do 20 za pomocą pętli for
+        for (Integer i = 0; i < 21; i = i + 2) {
+            System.out.println("Wypisuję liczbę parzystą: " + i);
+        }
+
+        //Zadanie 4: Wypisanie liczb nieparzystych od 100 do 0 za pomocą pętli for
+
+        for (Integer i = 100; i > 0; i--) { // nie moze byc tak i<0
+            System.out.println("Wypisuję liczbę nieparzystą: " + i);
+        }
+
     }
 }
